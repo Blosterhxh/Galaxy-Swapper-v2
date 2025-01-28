54209bfc8ec7cbe6ec7bcfe11e686f24c12b13a1
Bloster <simone.wveil@gmail.com>
2025-01-28 14:39:36 +0100

    Update Config.cs

--- Workspace/Properties/Config.cs
+++ Workspace/Properties/Config.cs
@@ -7,6 +7,7 @@ namespace Galaxy_Swapper_v2.Workspace.Properties
     {
         public static readonly string Path = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Galaxy-Swapper-v2-Config";
         public static readonly string[] Directories = { Path, $"{Path}\\DLLS", $"{Path}\\Plugins", $"{Path}\\LOGS" };
+        // Cette classe statique Config permet de gérer les configurations de l'application Galaxy Swapper V2 en créant des répertoires nécessaires et en nettoyant les configurations obsolètes.
         public static void Initialize()
         {
             if (File.Exists($"{Path}\\Key.config")) //Old Config Folder
